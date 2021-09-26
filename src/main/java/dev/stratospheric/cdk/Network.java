aa09cc908ff33b4c2474ab5c62f4c40b0d38e8a4
Tom Hombergs <tom.hombergs@gmail.com>
2021-09-26 12:00:33 +1000

    add "wither" method to optionally set sslCertificateArn

--- src/main/java/dev/stratospheric/cdk/Network.java
+++ src/main/java/dev/stratospheric/cdk/Network.java
@@ -419,19 +419,27 @@ public class Network extends Construct {
   }
 
   public static class NetworkInputParameters {
-    private final Optional<String> sslCertificateArn;
+    private Optional<String> sslCertificateArn;
 
     /**
      * @param sslCertificateArn the ARN of the SSL certificate that the load balancer will use
      *                          to terminate HTTPS communication. If no SSL certificate is passed,
      *                          the load balancer will only listen to plain HTTP.
+     * @deprecated use {@link #withSslCertificateArn(String)} instead
      */
+    @Deprecated
     public NetworkInputParameters(String sslCertificateArn) {
       this.sslCertificateArn = Optional.ofNullable(sslCertificateArn);
     }
 
     public NetworkInputParameters() {
       this.sslCertificateArn = Optional.empty();
+    }
+
+    public NetworkInputParameters withSslCertificateArn(String sslCertificateArn){
+      Objects.requireNonNull(sslCertificateArn);
+      this.sslCertificateArn = Optional.of(sslCertificateArn);
+      return this;
     }
 
     public Optional<String> getSslCertificateArn() {
